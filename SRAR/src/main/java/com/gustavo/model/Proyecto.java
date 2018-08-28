e66040d829ba177673bac0c8d468ed2fe3c8456f
Tavo12345 <gust.allg@gmail.com>
2018-08-27 19:11:44 -0500

    Modelo

---
+++ SRAR/src/main/java/com/gustavo/model/Proyecto.java
@@ -0,0 +1,59 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.gustavo.model;
+
+import java.io.Serializable;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+/**
+ *
+ * @author Gustavo
+ */
+
+@Entity
+@Table(name="proyecto")
+public class Proyecto implements Serializable {
+    
+    @Id
+    @GeneratedValue(strategy=GenerationType.IDENTITY)
+    private String codigoProyecto;
+    
+    @Column(name="nombreProyecto")
+    private String nombreProyecto;
+    
+    @Column(name="descripcion")
+    private String descripcion;
+
+    public String getCodigoProyecto() {
+        return codigoProyecto;
+    }
+
+    public void setCodigoProyecto(String codigoProyecto) {
+        this.codigoProyecto = codigoProyecto;
+    }
+
+    public String getNombreProyecto() {
+        return nombreProyecto;
+    }
+
+    public void setNombreProyecto(String nombreProyecto) {
+        this.nombreProyecto = nombreProyecto;
+    }
+
+    public String getDescripcion() {
+        return descripcion;
+    }
+
+    public void setDescripcion(String descripcion) {
+        this.descripcion = descripcion;
+    }
+    
+}
