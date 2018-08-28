420384112db2dd6b74f3f0f5a118029aed53ef48
Tavo12345 <gust.allg@gmail.com>
2018-08-27 19:27:17 -0500

    EJB

---
+++ SRAR/src/main/java/com/gustavo/ejb/CompromisoFacade.java
@@ -0,0 +1,31 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.gustavo.ejb;
+
+import com.gustavo.model.Compromiso;
+import javax.ejb.Stateless;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+
+/**
+ *
+ * @author Gustavo
+ */
+@Stateless
+public class CompromisoFacade extends AbstractFacade<Compromiso> implements CompromisoFacadeLocal {
+    @PersistenceContext(unitName = "primePU")
+    private EntityManager em;
+
+    @Override
+    protected EntityManager getEntityManager() {
+        return em;
+    }
+
+    public CompromisoFacade() {
+        super(Compromiso.class);
+    }
+    
+}
